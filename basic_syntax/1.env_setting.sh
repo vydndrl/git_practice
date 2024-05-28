16a6cb7ea41774cf15ab8d1cf105deaf46c93684
vydndrl <vydndrl1234@naver.com>
2024-05-28 17:47:46 +0900

    fix1

--- basic_syntax/1.env_setting.sh
+++ basic_syntax/1.env_setting.sh
@@ -7,4 +7,11 @@ git init
 
 # 원격지 주소를 생성 및 추가
 # 원격지 주소 추가
-git remote add origin <레포주소>
+git remote add origin 레포주소
+# 원격지 주소 삭제
+git remote remove origin
+# 원격 주소 변경
+git remote set-url origin 레포주소
+
+# git 설정정보 조회
+git config --list
