af6d9a82d099e2267924168bdfbd332a058ec585
vydndrl <vydndrl1234@naver.com>
2024-05-29 15:02:57 +0900

    vi commit this is vi commit

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -1,5 +1,17 @@
 # 현재 working directory, staging area 만 상태 확인
 git status
 
+# git add : 모든 수정/추가사항 add
+git add .
+
+# git add 특정 파일 : 특정 파일 add
+git add 특정파일(위치포함)
+
+# local repository 에 반영 및 커밋 이력 생성
+git commit -m "메시지타이틀" -m "메시지내용"
+
+# vi모드에서 작성 : git commit 후 엔터
+
+
 # commit 이력 확인(local repository의 이력)
 git log
