f3c8fc5b61f3ea47c6f50783e4228106338056f6
vydndrl <vydndrl1234@naver.com>
2024-05-29 15:16:59 +0900

    fix

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -15,6 +15,15 @@ git commit -m "메시지타이틀" -m "메시지내용"
 # add와 commit을 동시에
 git commit -am "add와 commit 동시에"
 
-
 # commit 이력 확인(local repository의 이력)
 git log
+# git log 간결하게
+git log --oneline
+
+# 전체 이력 조회
+git log --all
+
+# 원격저장소로 업로드
+git push origin 브랜치명
+
+# 충돌무시하고 강제 push
