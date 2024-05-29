0c58aa2bdf1c78f73b1fcf756c3b4922fedd488b
vydndrl <vydndrl1234@naver.com>
2024-05-29 16:09:14 +0900

    fix

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -38,3 +38,15 @@ git checkout 커밋ID
 git checkout master
 
 # git pull은 원격의 변경사항을 local로 다운로드
+git pull origin 브랜치명
+
+# git 작업 취소
+# working directory에서의 취소
+git checkout .
+# staging area에서의 취소
+git reset
+# commit 이후의 취소
+git reset HEAD~1
+git reset HEAD^
+# push 이후의 취소
+git revert 커밋ID
